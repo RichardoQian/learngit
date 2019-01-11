d42acb0c92f54daf01663a7ada7fa139aa39d269
RichardoQian <40288528+RichardoQian@users.noreply.github.com>
2019-01-11 13:05:20 +0800

    Delete CommonTransform.h

--- CommonTransform.h
+++
@@ -1,9 +0,0 @@
-#ifndef COMMON_TRANSFORM_H
-#define COMMON_TRANSFORM_H
-
-/// for net
-unsigned inSize(unsigned k, unsigned s, unsigned pad, unsigned outSize);
-unsigned outSize(unsigned k, unsigned s, unsigned pad, unsigned inSize);
-unsigned poolOutSize(unsigned k, unsigned s, unsigned pad, unsigned inSize);
-
-#endif
